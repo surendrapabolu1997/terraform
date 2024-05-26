a68d3d0cb6630b821f82f98fe77a040d811330e2
Your Name <you@example.com>
2024-05-26 23:17:35 +0530

    new

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,8 +1,40 @@
-resource "aws_instance" "demo-vm" {
-  ami           = ami-08564adb5a86605e7
-  instance_type = "t3.micro"
+# Creates EC2
+resource "aws_instance" "server" {
+  ami                     = "ami-0c1d144c8fdd8d690"
+  instance_type           = "t3.nano"
+  vpc_security_group_ids  = [aws_security_group.allow_all.id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "TerraformServer"
+  }
+}
+
+output "private_ip_address" {
+  value = aws_instance.server.private_ip
+}
+
+
+# Creates a Security Group
+resource "aws_security_group" "allow_all" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
   }
 }
