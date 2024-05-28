a07b5c08b55a76e7d08c73866fd7ab205c1d9f17
Your Name <you@example.com>
2024-05-29 01:38:24 +0530

    new

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,6 @@
 # Creates EC2
 resource "aws_instance" "server" {
-  ami                     = "ami-0c1d144c8fdd8d690"
+  ami                     = "ami-086ae3f3617c43822"
   instance_type           = "t3.nano"
   vpc_security_group_ids  = [aws_security_group.allow_all.id]
 
@@ -21,8 +21,8 @@ resource "aws_security_group" "allow_all" {
 
   ingress {
     description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
+    from_port   = 0
+    to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
